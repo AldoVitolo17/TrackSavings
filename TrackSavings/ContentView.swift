d0ec76f538485791641717c1d80814e40eddb6e7
AldoVitolo17 <aldo.vitolo1910@gmail.com>
2024-02-13 15:25:56 +0100

    Initial Commit

---
+++ TrackSavings/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrackSavings
+//
+//  Created by Aldo Vitolo on 13/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrackSavings/TrackSavingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackSavingsApp.swift
+//  TrackSavings
+//
+//  Created by Aldo Vitolo on 13/02/24.
+//
+
+import SwiftUI
+
+@main
+struct TrackSavingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
