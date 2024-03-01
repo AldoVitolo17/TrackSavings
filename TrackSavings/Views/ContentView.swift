4a683f3c1d33e9f77d526eb84f3f2b8fcf512a59
Isaac Lopez <isaac.lopez.1b@gmail.com>
2024-03-01 15:32:31 +0100

    Notificaion badge number

--- TrackSavings/Views/ContentView.swift
+++ TrackSavings/Views/ContentView.swift
@@ -120,6 +120,9 @@ struct ContentView: View {
                 .ignoresSafeArea(edges: .bottom)
             }
         }
+        .onAppear{
+            UIApplication.shared.applicationIconBadgeNumber = 0
+        }
     }
 }
 
